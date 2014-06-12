a6072ca2828d61da83ef30b3bec698fb0ae17527
Dan Kleiman <info@dankleiman.com>
2014-06-12 15:42:13 -0400

    Posted new question.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,24 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to "/questions/#{@question.id}"
+    else
+      flash.now[:notice] = "Could not save your question."
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
 end
