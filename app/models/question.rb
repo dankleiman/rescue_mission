13c58834294ecf14e00a5c876e411013c725732f
Dan Kleiman <info@dankleiman.com>
2014-06-12 16:17:14 -0400

    Post a question and return to index completed.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,9 @@ class Question < ActiveRecord::Base
 
   validates :title,
   presence: true,
-  length: { minimum: 40 }
+  length: { minimum: 40, too_short: "Must be at least 40 characters long." }
 
   validates :description,
   presence: true,
-  length: { minimum: 150 }
+  length: { minimum: 150, too_short: "Must be at least 150 characters long."  }
 end
