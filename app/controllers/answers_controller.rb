be1cf5cb863fae56d9e59a948527079459276054
Dan Kleiman <info@dankleiman.com>
2014-06-13 16:00:14 -0400

    Mark best answer.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,6 +12,20 @@ class AnswersController < ApplicationController
     end
   end
 
+  def update
+    answer = Answer.where(best_answer: true).first
+    if answer
+      answer.best_answer = false
+      answer.save
+    end
+
+    answer = Answer.find(params[:id])
+    answer.best_answer = true
+    answer.save
+
+    redirect_to "/questions/#{params[:question_id]}"
+  end
+
   private
 
   def answer_params
